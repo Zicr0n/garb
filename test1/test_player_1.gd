7858ac9375c52fa7f89723a1ce2e4b7e622ac74e
Poppegon <alvinboy0705@gmail.com>
2025-09-17 08:51:37 +0200

    spelarvariabler

--- test1/test_player_1.gd
+++ test1/test_player_1.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+@export var maxRunSpeed : int = 3
+@export var runAcceleration : int = 1
+@export var jumpPower : int = 5
+@export var wallSLideSpeed : int = 1
+@export var wallJumpVerticalPower : int = 3
+@export var wallJumpHorizontalPower : int = 4
