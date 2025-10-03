90217978a5e49f425597c250c6f4466579949c29
Poppegon <alvinboy0705@gmail.com>
2025-10-03 11:57:24 +0200

    mainog

--- menus/level_select_buttons.gd
+++ menus/level_select_buttons.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @export var main_menu : MainMenu = null
+@export var level_1 = null
 
 func _on_level_1_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://test-scene.tscn")
 
 
 func _on_back_button_pressed() -> void:
