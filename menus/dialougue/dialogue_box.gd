12f057780e2744a3cab849fd5b19737ae607a0dd
Poppegon <alvinboy0705@gmail.com>
2025-11-09 19:34:15 +0100

    Klart för testing
    Ska läggas till:
    Avaktivera spelarkontroll under dialog
    Kanske snyggare text med egen font - (oviktigt)
    Ljudeffekter - (inte jättteviktigt men mycket viktigare än egen font)
    Kanske porträtt - (inte heller viktigt)

--- menus/dialougue/dialogue_box.gd
+++ menus/dialougue/dialogue_box.gd
@@ -1,20 +1,33 @@
 extends Control
 
+signal dialogue_finished
+
 @export var dialogue : Array[String] = []
 @export var text_label : Label = null
 @export var next_label : Label = null
 
 var current_line : String = ""
 var shown_text : String = ""
+
 var i : int = 0
 var ii : int = 0
+
+var letter_delay := 0.03
+var letter_timer := 0.0
 
 func _ready() -> void:
 	next_label.visible = false
 	change_line_to()
 
-func _process(_delta) -> void:
-	add_letter()
+func _process(delta) -> void:
+	letter_timer += delta
+	if letter_timer >= letter_delay:
+		add_letter()
+		letter_timer = 0.0
+
+	if Input.is_action_just_pressed("jump"):
+		shown_text = current_line
+		i = current_line.length()
 
 	if next_label.visible and Input.is_action_just_pressed("jump"):
 		next_line()
@@ -34,13 +47,15 @@ func next_line() -> void:
 	i = 0
 	ii += 1
 	change_line_to()
+	next_label.visible = false
 
 func change_line_to() -> void:
-	if ii < (dialogue.size() - 1):
+	if ii < dialogue.size():
 		current_line = dialogue[ii]
 		empty_box()
 	else:
-		return# dialogue is done
+		dialogue_finished.emit()
+		get_tree().root.queue_free()
 
 func empty_box() -> void:
 	shown_text = ""
