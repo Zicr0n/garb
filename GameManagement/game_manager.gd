f699e4e56c14fd56981442b5b24fd84138acdc87
Zicr0n <alwanisamir007@gmail.com>
2025-11-20 15:22:56 +0100

    Game manager

--- GameManagement/game_manager.gd
+++ GameManagement/game_manager.gd
@@ -2,7 +2,19 @@ extends Node
 
 signal dim_screen
 
+@export var levels = {
+	"level1" : "res://Levels/level_1.tscn"
+}
+
 ## TODO - Make screen go dim (cool transition perhaps), reload scene, reposition at correct checkpoint, brighten screen again
 func player_died(_player : Player):
 	print("boohoo noob")
 	dim_screen.emit()
+
+func load_level(sceneName):
+	var scene = levels[sceneName]
+	
+	if scene:
+		get_tree().change_scene_to_file(scene)
+	else:
+		push_error("SCENE NOT FOUND!!!!!!!!!!!")
