848261a323b97c7fbd9bec8eee2b8a4bd4a32af6
Poppegon <alvinboy0705@gmail.com>
2025-12-01 11:21:04 +0100

    Yank nästa

--- Levels/Level Parts/Scripts/bounce_pad.gd
+++ Levels/Level Parts/Scripts/bounce_pad.gd
@@ -2,7 +2,10 @@ extends Area2D
 
 @export var bounce_power = 500
 
+var player = null
+
 func _on_body_entered(body: Node2D) -> void:
 	var direction = Vector2(cos(rotation - (PI/2)), sin(rotation - (PI/2)))
 	print(rotation, direction)
 	body.statemachine.move_component.custom_set_velocity((direction * bounce_power))
+	player = body
