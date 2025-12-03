2b1972e4db42a86aa1f0d0bba83ee4c8a08a2faa
Zicr0n <alwanisamir007@gmail.com>
2025-12-03 15:13:37 +0100

    fixed bounce

--- Levels/Level Parts/Scripts/btn_door.gd
+++ Levels/Level Parts/Scripts/btn_door.gd
@@ -1,10 +1,13 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
 @export var button : Area2D = null
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
-	$CollisionShape2D.disabled = false
+	collision_shape_2d.disabled = false
 	button.pressed.connect(activate)
 
 func activate() -> void:
-	$CollisionShape2D.disabled = true
+	collision_shape_2d.disabled = true
+	collision_shape_2d.set_deferred("disabled", true)
+	
