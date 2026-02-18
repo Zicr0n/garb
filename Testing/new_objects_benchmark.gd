ef7f70a6349b5a590d8fb0461813327df66c8184
Zicr0n <alwanisamir007@gmail.com>
2026-02-18 12:09:36 +0100

    i pool

--- Testing/new_objects_benchmark.gd
+++ Testing/new_objects_benchmark.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
-@export var intensity_multiplier : float = 0
+@export var intensity_multiplier : float = 1.0
+@export var scene_to_instantiate : PackedScene = null
+@export var preload_amount : int = 10
+@export var parent = self
+var background_pool = []
+var current_pool = []
+
+func _ready() -> void:
+	for i in range(preload_amount):
+		var instantiated = scene_to_instantiate.instantiate()
+		instantiated.process_mode = Node.PROCESS_MODE_DISABLED
+		instantiated.set_physics_process(false)
+		instantiated.global_position = Vector2(randf_range(0,640), 30)
+		#instantiated.hidden = true
+		parent.add_child(instantiated)
+		background_pool.append(instantiated)
+
+func callObject(index):
+	pass
